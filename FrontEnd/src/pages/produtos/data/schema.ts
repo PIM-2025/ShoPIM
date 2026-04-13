c82e946f2128bd82b2307d4907b8cbb99743f398
Jean Campos <jeanflaviodecampos1604@gmail.com>
2026-04-13 18:21:40 -0300

    correção erro deploy

--- FrontEnd/src/pages/produtos/data/schema.ts
+++ FrontEnd/src/pages/produtos/data/schema.ts
@@ -7,7 +7,7 @@ export const produtoSchema = z.object({
   categoria: z.string(),
   quantidade: z.coerce.number(),
   imagem: z.string().optional().default(''),
-  descricaoDetalhada: z.string().optional().default(''),
+  descricaoDetalhada: z.string().optional(),
 })
 
 export type Produto = z.infer<typeof produtoSchema>
